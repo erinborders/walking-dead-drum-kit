59ed66f5378fb98af5249ee0934bf8b0a69157c3
Erin Borders <borders16@gmail.com>
2019-06-26 20:23:12 -0400

    created a script.js file and linked it in my index.html. made an array of objects containing the info for the drum kit keys

---
+++ script.js
@@ -0,0 +1,38 @@
+const drumKitTiles = [
+    {
+        dataKey: 65,
+        sound: "example"
+    },
+    {
+        dataKey: 83,
+        sound: "example"
+    },
+    {
+        dataKey: 68,
+        sound: "example"
+    },
+    {
+        dataKey: 70,
+        sound: "example"
+    },
+    {
+        dataKey: 71,
+        sound: "example"
+    },
+    {
+        dataKey: 72,
+        sound: "example"
+    },
+    {
+        dataKey: 74,
+        sound: "example"
+    },
+    {
+        dataKey: 75,
+        sound: "example"
+    },
+    {
+        dataKey: 76,
+        sound: "example"
+    }
+]
